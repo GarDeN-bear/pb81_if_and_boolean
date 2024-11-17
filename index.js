81493750560ceceea4ee85996ec737bf6b4046d7
GarDeN-bear <96305662+GarDeN-bear@users.noreply.github.com>
2024-11-17 17:24:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -15,4 +15,35 @@ function task1(name, balance) {
   }
 }
 
+function task2(user, orderOwner) {
+  if (user === orderOwner || user == 'admin') {
+    console.log('Редактирование разрешено');
+  } else {
+    console.log('Заказ нельзя редактировать');
+  }
+}
+
+function task3(
+  bonusBalance,
+  countPurchases,
+  standardAccrualInPer = 10,
+  increasedAccuralInPer = 20,
+  supplementsForPurchasesInPer = 5
+) {
+  let countPer = 0;
+  if (bonusBalance > 5000) {
+    countPer += increasedAccuralInPer;
+  } else {
+    countPer += standardAccrualInPer;
+  }
+
+  if (countPurchases > 1) {
+    countPer += supplementsForPurchasesInPer;
+  }
+
+  console.log('Процент покупки: ' + countPer);
+}
+
 task1('Vova', 9000);
+task2('netologist', 'leo_tolstoy');
+task3(500, 2);
